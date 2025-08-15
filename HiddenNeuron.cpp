723faa62d8227f05b559e8776affec99a682d17c
BigDrilla <107286583+Cypherr25@users.noreply.github.com>
2025-08-15 07:08:15 -0400

    Add files via upload

--- HiddenNeuron.cpp
+++ HiddenNeuron.cpp
@@ -4,14 +4,14 @@
 
 void HiddenNeuron::Update(const std::vector<double>& inputs, HiddenNeuronParams& params, const double learningRate, const std::vector<double>& outputDeltas, const std::vector<double>& weightsToOutputs) {
     double totalDelta = 0.0;
-    for (size_t j = 0; j < outputDeltas.size(); j++) {
-        totalDelta += outputDeltas[j] * weightsToOutputs[j];
+    for (int i = 0; i < outputDeltas.size(); i++) {
+        totalDelta += outputDeltas[i] * weightsToOutputs[i];
     }
 
     double delta = totalDelta * params.output * (1.0 - params.output);
 
     // Update each input weight
-    for (size_t i = 0; i < params.weights.size(); i++) {
+    for (int i = 0; i < params.weights.size(); i++) {
         params.weights[i] -= learningRate * delta * inputs[i];
     }
 
@@ -21,7 +21,7 @@ void HiddenNeuron::Update(const std::vector<double>& inputs, HiddenNeuronParams&
 
 void HiddenNeuron::CalcOutput(const std::vector<double>& inputs, HiddenNeuronParams& params) {
     double sum = params.bias;
-    for (size_t i = 0; i < inputs.size(); i++) {
+    for (int i = 0; i < inputs.size(); i++) {
         sum += inputs[i] * params.weights[i];
     }
     params.output = CalcSigmoid(sum);
